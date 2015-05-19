6e4c4033873ac7c529c22ed0f5091417da4dcbca
carlo.vanhoutte@gmail.com <carlo.vanhoutte@gmail.com>
2015-05-19 16:20:51 +0100

    Added basic app and index page

--- SPWebApplication/SPFrontEnd/js/app.js
+++ SPWebApplication/SPFrontEnd/js/app.js
@@ -1,1 +1,30 @@
-﻿
+﻿(function () {
+    var app = angular.module('pokerShore', []);
+
+    app.controller('SelectEstimateController', ['$scope', '$http', function ($scope, $http) {
+        $scope.addEstimateToBPI = function () {	
+            var dataObj = {
+                /*
+                pbi_id: $scope.pbi_id,
+                participant_hash: $scope.participant_hash,
+                estimate: $scope.estimate
+                */
+                pbi_id: '123',
+                participant_hash: 'ABC123',
+                estimate: 8
+            };
+
+            var res = $http.post('/', dataObj);
+
+            res.success(function (data, status, headers, config) {
+                $scope.message = data;
+                console.log('Succeeded to post estimate. Statuscode: ' + status);
+            });
+
+            res.error(function (data, status, headers, config) {
+                console.log('Failed to post estimate. Statuscode: ' + status);
+            });
+        };
+    }]);
+    
+})();
