3a6f1dccc9bb2a92f0e20cdc24200dc7204dc00b
LLambrec <laurensl@live.be>
2015-06-01 20:39:59 +0200

    DAO files

---
+++ SPWebApplication/SPBackEnd/DAO/DAOParticipant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using SPBackEnd.Context;
+using SPBackEnd.DTO;
+
+namespace SPBackEnd.DAO
+{
+    public class DAOParticipant : DAOBase<MainDbContext>
+    {
+        
+    }
+       
+}
