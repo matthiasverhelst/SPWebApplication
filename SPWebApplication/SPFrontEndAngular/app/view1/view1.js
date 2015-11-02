ebb4878786b97af08c19e867b29d5eb5431d0a76
carlo.vanhoutte@gmail.com <carlo.vanhoutte@gmail.com>
2015-11-02 18:30:10 +0100

    Update frontend architecture to Angular bootstrap

---
+++ SPWebApplication/SPFrontEndAngular/app/view1/view1.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function () {
+  angular.module('myApp.view1', ['ngRoute'])
+
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    });
+  }])
+
+  .controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+      $scope.pbi = {
+        id: '123',
+        userHash: 'ABC123',
+        estimate: undefined
+      };
+
+      $scope.addEstimateToPBI = function () {
+
+        var res = $http.post('http://localhost:50529/TestService.svc/AddEstimate', $scope.pbi);
+
+        res.success(function (data, status, headers, config) {
+          $scope.message = data;
+          console.log('Succeeded to post estimate. Statuscode: ' + status);
+        });
+
+        res.error(function (data, status, headers, config) {
+          console.log('Failed to post estimate. Statuscode: ' + status);
+        });
+      };
+  }]);
+})();
