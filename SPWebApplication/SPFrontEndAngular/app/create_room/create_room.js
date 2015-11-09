d5fe1d53f9bf09df3158d9c1e91c543d2e439c55
carlo.vanhoutte@gmail.com <carlo.vanhoutte@gmail.com>
2015-11-09 19:29:53 +0100

    Add create room

---
+++ SPWebApplication/SPFrontEndAngular/app/create_room/create_room.js
@@ -0,0 +1,39 @@
+'use strict';
+
+(function () {
+  angular.module('myApp.createRoom', ['ngRoute'])
+
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/create_room', {
+      templateUrl: 'create_room/create_room.html',
+      controller: 'CreateRoomCtrl'
+    });
+  }])
+
+  .controller('CreateRoomCtrl', ['$scope', '$http', function($scope, $http) {
+      $scope.listOfRooms = [];  
+      $scope.addRoom = function(){
+          console.log("addRoom called")
+    
+          if(!$scope.roomInput || $scope.roomInput === '') { return; }
+
+          $scope.listOfRooms.push({roomName: $scope.roomInput, roomOwner: "Scrum Master"});
+          $scope.roomInput='';    
+      };  
+  
+      $scope.validateEmail = function(email2){
+          /*console.log("validate email called :" + $scope.GregTest);
+          console.log("validate email called2 :" + $scope.email2);*/          
+                               
+          var patt = new RegExp("[a-zA-Z0-9]+\@[a-zA-Z0-9]+(.[a-zA-Z0-9]+)+");
+          var res = patt.test($scope.GregTest);
+
+          if (res == false) {
+              window.alert("Invalid e-mail address ('" + $scope.GregTest + "'). Please, correct it.");
+          }
+                
+          /*window.alert("RES : '" + res + "' for '" + $scope.GregTest + "'");  */
+          return res;
+      };
+  }]);
+})();
