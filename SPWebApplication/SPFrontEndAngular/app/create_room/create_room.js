e54abaa7afb8211b29770e752c5078c932d9874c
carlo.vanhoutte@gmail.com <carlo.vanhoutte@gmail.com>
2015-11-30 18:46:03 +0100

    Updated and simplified create_room

--- SPWebApplication/SPFrontEndAngular/app/create_room/create_room.js
+++ SPWebApplication/SPFrontEndAngular/app/create_room/create_room.js
@@ -13,20 +13,26 @@
   .controller('CreateRoomCtrl', ['$scope', '$http', function($scope, $http) {
       $scope.listOfRooms = [];  
       $scope.addRoom = function(){
-          console.log("addRoom called")
+          console.log("addRoom called");
     
           if(!$scope.roomInput || $scope.roomInput === '') { return; }
 
           $scope.listOfRooms.push({roomName: $scope.roomInput, roomOwner: "Scrum Master"});
-          $scope.roomInput='';    
+          $scope.roomInput='';
       };  
   
-      $scope.validateEmail = function(email2){                               
+      $scope.validateEmail = function(){                               
           var patt = new RegExp("[a-zA-Z0-9]+\@[a-zA-Z0-9]+(.[a-zA-Z0-9]+)+");
-          var res = patt.test($scope.GregTest);
+          var res = patt.test($scope.email);
 
           if (res == false) {
-              window.alert("Invalid e-mail address ('" + $scope.GregTest + "'). Please, correct it.");
+              //window.alert("Invalid e-mail address ('" + $scope.email + "'). Please, correct it.");
+              $scope.emailIncorrect = true;
+          }
+
+          else {
+              $scope.emailIncorrect = false;
+              $scope.addRoom();
           }
           return res;
       };
