c56deb610e42f88a50181f87780b578b2b16c7be
gregorynholland <gregory.holland@capgemini.com>
2015-11-30 20:15:50 +0100

    To test UnitTest

---
+++ SPWebApplication/SPFrontEndAngular/for-unit-test/MyClassToTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/* HOW TO CREATE A UNIT TEST : https://www.visualstudio.com/en-us/get-started/code/create-and-run-unit-tests-vs */
+
+namespace SPFrontEndAngular
+{
+    public class MyClassToTest
+    {
+        public int square(int number)
+        {
+            return number * number;
+        }
+
+    }
+}
