aff22341b3cd22b9afc5a8d1d866fe94e5c48310
bramdufour <bram.dufour8@gmail.com>
2016-02-15 20:27:53 +0100

    voting screen view added

---
+++ SPWebApplication/SPFrontEndAngular/app/views/voting_screen/voting_screen.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+(function () {
+    angular.module('pokerShoreApp.voting_screen', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/voting_screen', {
+            templateUrl: 'views/voting_screen/voting_screen.html',
+            controller: 'VotingCtrl'
+        });
+    }])
+
+    .controller('VotingCtrl', ['$scope', function ($scope) {
+        
+    }]);
+})();
