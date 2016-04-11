4437078f70475ff95796c61fe6841a50e3d49886
PBEELEN <pieter-jan.beelen@capgemini.com>
2016-04-11 20:20:31 +0200

    no message

--- SPWebApplication/SPFrontEndAngular/app/views/scrumMaster/waitingRoom/waitingRoom.js
+++ SPWebApplication/SPFrontEndAngular/app/views/scrumMaster/waitingRoom/waitingRoom.js
@@ -1,56 +1,17 @@
 'use strict';
 
 (function () {
-    angular.module('pokerShoreApp.home', ['ngRoute'])
+  angular.module('pokerShoreApp.waitingRoomScrumMaster', ['ngRoute'])
 
   .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {
-      templateUrl: 'views/home/home.html',
-      controller: 'HomeCtrl'
+    $routeProvider.when('/waitingRoomScrumMaster/:param1', {
+      templateUrl: 'views/scrumMaster/waitingRoom/waitingRoom.html',
+      controller: 'waitingRoomScrumMasterCtrl'
     });
   }])
 
-  .controller('HomeCtrl', ['$scope', '$http', '$location', '$rootScope', 'signalRSvc','$timeout', function ($scope, $http, $location, $rootScope, signalRSvc, $timeout) {
-      $scope.createRoomObj = {};
-      $scope.joinRoomObj = {};
-      $scope.errors = {};
-      $scope.createRoomFormSubmitted = false;
-      $scope.joinRoomFormSubmitted = false;
-
-      $scope.createRoom = function () {
-         $scope.createRoomFormSubmitted = true;
-         signalRSvc.createRoom($scope.createRoomObj.scrumMasterName);
-      };
-
-      $scope.joinRoom = function(){
-          $scope.errors={};
-          $scope.joinRoomFormSubmitted = true;
-          if($scope.joinRoomObj.roomId && $scope.joinRoomObj.scrumMemberName && $scope.joinRoomObj.roomId !== "" && $scope.joinRoomObj.scrumMemberName !== ""){
-              signalRSvc.joinRoom($scope.joinRoomObj.scrumMemberName,$scope.joinRoomObj.roomId);
-          }
-      };
-
-
-      PubSub.subscribe( 'roomJoined', function(msg, success){
-          console.log("success", success);
-          if(success){
-              $scope.errors.invalidRoomId = false;
-              var roomPath = '/room/' + $scope.joinRoomObj.roomId;
-              $location.path(roomPath);
-          }else{
-              $scope.errors.invalidRoomId = true;
-          }
-          $timeout(function(){
-              $scope.$apply();
-          },0);
-      });
-
-      PubSub.subscribe( 'roomCreated', function(msg, roomId){
-          var roomPath = '/room/' + roomId;
-          $location.path(roomPath);
-          $timeout(function(){
-              $scope.$apply();
-          },0);
-      });
+  .controller('waitingRoomScrumMasterCtrl', ['$scope','$routeParams', '$http', function($scope, $routeParams, $http) {
+      var roomId = $routeParams;
+      $scope.roomNum = roomId.param1;
   }]);
 })();
--- SPWebApplication/SPFrontEndAngular/app/views/scrumMember/waitingRoom/waitingRoom.js
+++ SPWebApplication/SPFrontEndAngular/app/views/scrumMember/waitingRoom/waitingRoom.js
@@ -1,56 +1,17 @@
 'use strict';
 
 (function () {
-    angular.module('pokerShoreApp.home', ['ngRoute'])
+  angular.module('pokerShoreApp.waitingRoomScrumMember', ['ngRoute'])
 
   .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {
-      templateUrl: 'views/home/home.html',
-      controller: 'HomeCtrl'
+    $routeProvider.when('/waitingRoomScrumMember/:param1', {
+      templateUrl: 'views/scrumMember/waitingRoom/waitingRoom.html',
+      controller: 'waitingRoomScrumMemberCtrl'
     });
   }])
 
-  .controller('HomeCtrl', ['$scope', '$http', '$location', '$rootScope', 'signalRSvc','$timeout', function ($scope, $http, $location, $rootScope, signalRSvc, $timeout) {
-      $scope.createRoomObj = {};
-      $scope.joinRoomObj = {};
-      $scope.errors = {};
-      $scope.createRoomFormSubmitted = false;
-      $scope.joinRoomFormSubmitted = false;
-
-      $scope.createRoom = function () {
-         $scope.createRoomFormSubmitted = true;
-         signalRSvc.createRoom($scope.createRoomObj.scrumMasterName);
-      };
-
-      $scope.joinRoom = function(){
-          $scope.errors={};
-          $scope.joinRoomFormSubmitted = true;
-          if($scope.joinRoomObj.roomId && $scope.joinRoomObj.scrumMemberName && $scope.joinRoomObj.roomId !== "" && $scope.joinRoomObj.scrumMemberName !== ""){
-              signalRSvc.joinRoom($scope.joinRoomObj.scrumMemberName,$scope.joinRoomObj.roomId);
-          }
-      };
-
-
-      PubSub.subscribe( 'roomJoined', function(msg, success){
-          console.log("success", success);
-          if(success){
-              $scope.errors.invalidRoomId = false;
-              var roomPath = '/room/' + $scope.joinRoomObj.roomId;
-              $location.path(roomPath);
-          }else{
-              $scope.errors.invalidRoomId = true;
-          }
-          $timeout(function(){
-              $scope.$apply();
-          },0);
-      });
-
-      PubSub.subscribe( 'roomCreated', function(msg, roomId){
-          var roomPath = '/room/' + roomId;
-          $location.path(roomPath);
-          $timeout(function(){
-              $scope.$apply();
-          },0);
-      });
+  .controller('waitingRoomScrumMemberCtrl', ['$scope','$routeParams', '$http', function($scope, $routeParams, $http) {
+      var roomId = $routeParams;
+      $scope.roomNum = roomId.param1;
   }]);
 })();
