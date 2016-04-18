578372fd232c154ae6455c8f314b44665366072e
PBEELEN <pieter-jan.beelen@capgemini.com>
2016-04-18 19:58:33 +0200

    no message

--- SPWebApplication/SPFrontEndAngular/app/views/scrumMaster/waitingRoom/waitingRoom.js
+++ SPWebApplication/SPFrontEndAngular/app/views/scrumMaster/waitingRoom/waitingRoom.js
@@ -14,6 +14,40 @@
       var roomId = $routeParams;
       $scope.roomNum = roomId.param1;
       $scope.participantsList = [];
+      $scope.createPbi = function (newPbi) {
+          console.log("createPbi");
+          if (newPbi && newPbi.length >= 1) {
+              var Pbi = {
+                  "PBI_name": newPbi,
+                  "PBI_score": ""
+              };
+              $scope.pbiArray.push(Pbi);
+              if ($scope.setPbiFocus) {
+                  focusNewPbi();
+              }
+          }
+      };
+
+      var focusNewPbi = function () {
+          console.log("focus");
+          document.getElementById("newPbi").focus();
+          $scope.setPbiFocus = false;
+      }
+
+      $scope.submit = function () {
+          console.log("submit");
+          document.activeElement.blur();
+          //document.getElementById("newPbi").blur();
+          focusNewPbi();
+      };
+
+
+      $scope.removePbi = function (index) {
+          $scope.pbiArray.splice(index, 1);
+      };
+
+      $scope.pbiArray = [];
+
 
 
       PubSub.subscribe( 'participantsListChanged', function(msg, participantsList){
