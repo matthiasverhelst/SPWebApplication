a8fd41cc4310cf4b41737cc0e77790f15b2b6c8b
PBEELEN <pieter-jan.beelen@capgemini.com>
2016-04-25 21:04:35 +0200

    clean version of sending and receiving signlar messages

--- SPWebApplication/SPFrontEndAngular/app/app.js
+++ SPWebApplication/SPFrontEndAngular/app/app.js
@@ -16,9 +16,18 @@
 
     pokerShoreApp.value('$', $);
 
-    pokerShoreApp.service('signalRSvc', ['$', '$rootScope', function ($, $rootScope) {
+    pokerShoreApp.service('signalRSvc', ['$', '$rootScope','$timeout', function ($, $rootScope,$timeout) {
         var proxy = null;
         var roomId = "";
+
+        var CONST = {
+            CREATE_ROOM : 'createRoom',
+            JOIN_ROOM : 'joinRoom',
+            GET_PARTICIPANTS : 'GetParticipants',
+            GET_PBIS : 'GetPBIs',
+            CREATE_PBI : 'createPBI',
+            REMOVE_PBI : 'removePBI'
+        };
 
         var initialize = function () {
             //Getting the connection object
@@ -27,26 +36,20 @@
             //Creating proxy
             this.proxy = connection.createHubProxy('scrumPokerHub');
 
-            this.proxy.on('roomCreated', function (id) {
-                roomId = id;
-                PubSub.publish( 'roomCreated', id );
-            });
+            var createProxyListener = function(proxyObj, proxyID){
+                proxyObj.on(proxyID, function (obj){
+                    if(proxyID === "roomCreated"){
+                        roomId = obj;
+                    }
+                    PubSub.publish(proxyID, obj );
+                });
+            };
 
-            this.proxy.on('roomJoined', function (success) {
-                PubSub.publish( 'roomJoined', success );
-            });
+            var onProxyVarsArray = ['roomCreated', 'roomJoined', 'getParticipants', 'getPBIS'];
 
-            this.proxy.on('getParticipants', function (participantsList) {
-                PubSub.publish( 'participantsListChanged', participantsList );
-            });
-
-            this.proxy.on('getPBIS', function (pbiArray) {
-                console.log("getPBIS");
-                console.log(pbiArray);
-                PubSub.publish( 'getPBIList', pbiArray );
-            });
-
-
+            for(var i = 0; i < onProxyVarsArray.length; i++){
+                createProxyListener(this.proxy, onProxyVarsArray[i]);
+            }
 
             //Starting connection
             connection.start().done(function () {
@@ -55,52 +58,25 @@
                 console.log(e);
                 console.log("Connection Failed.");
             });
+        }
+
+        var sendRequest = function (reqName, obj) {
+            this.proxy.invoke(reqName, obj);
         };
 
-
-
-
-        var sendRequest = function (name, message) {
-            //Invoking greetAll method defined in hub
-            this.proxy.invoke('send', name, message);
+        var sendRequestWithRoomID = function (reqName, obj) {
+            if (obj){
+                this.proxy.invoke(reqName, roomId, obj);
+            }else{
+                this.proxy.invoke(reqName, roomId);
+            }
         };
-
-        var createRoom = function (name) {
-            this.proxy.invoke('createRoom', name);
-        };
-
-        var joinRoom = function (name, id) {
-            roomId = id;
-            this.proxy.invoke('joinRoom', name, id);
-        };
-
-        var getParticipants = function () {
-            this.proxy.invoke('getParticipants', roomId);
-        };
-
-        var createPBI = function (pbiName) {
-            this.proxy.invoke('createPBI', roomId, pbiName);
-        };
-
-        var removePBI = function (pbiName) {
-            this.proxy.invoke('removePBI', roomId, pbiName);
-        };
-
-        var getPBIS = function () {
-            this.proxy.invoke('getPBIS', roomId);
-        };
-
-
 
         return {
             initialize: initialize,
+            CONST : CONST,
             sendRequest: sendRequest,
-            createRoom: createRoom,
-            joinRoom: joinRoom,
-            getParticipants: getParticipants,
-            createPBI: createPBI,
-            removePBI: removePBI,
-            getPBIS : getPBIS
+            sendRequestWithRoomID : sendRequestWithRoomID
         };
     }]);
 
@@ -109,8 +85,6 @@
     }]);
 
     pokerShoreApp.controller('mainController', ['$scope', 'signalRSvc', function ($scope, signalRSvc) {
-
         signalRSvc.initialize();
-
     }]);
 })();
