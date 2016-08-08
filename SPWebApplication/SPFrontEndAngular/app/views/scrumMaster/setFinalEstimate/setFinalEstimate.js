a1fe606fc956484ef32025040c8967cccfdee63f
PBEELEN <pieter-jan.beelen@capgemini.com>
2016-08-08 19:51:34 +0200

    Applied new styling everywhere Still a few need new styling

--- SPWebApplication/SPFrontEndAngular/app/views/scrumMaster/setFinalEstimate/setFinalEstimate.js
+++ SPWebApplication/SPFrontEndAngular/app/views/scrumMaster/setFinalEstimate/setFinalEstimate.js
@@ -13,7 +13,7 @@
     .controller('SetFinalEstimateCtrl', ['$scope', '$location', '$timeout', '$routeParams', 'signalRSvc', function ($scope, $location, $timeout, $routeParams, signalRSvc) {
         $scope.pbiName = $routeParams.pbi;
         $scope.buttons = [
-            { value: '0', text: '0' }, { value: '0.5', text: '0.5' }, { value: '1', text: '1' },
+            { value: '0', text: '0' }, { value: '0.5', text: '1/2' }, { value: '1', text: '1' },
             { value: '2', text: '2' }, { value: '3', text: '3' }, { value: '5', text: '5' },
             { value: '8', text: '8' }, { value: '13', text: '13' }, { value: '20', text: '20' },
             { value: '40', text: '40' }, { value: '100', text: '100' }, { value: '?', text: '?' },
