84cc447f4ce9016e541727b01caf02fae0cbd4f8
PBEELEN <pieter-jan.beelen@capgemini.com>
2016-12-05 20:44:00 +0100

    Added logging

--- SPWebApplication/SPFrontEndAngular/Hubs/ScrumPokerHub.cs
+++ SPWebApplication/SPFrontEndAngular/Hubs/ScrumPokerHub.cs
@@ -18,7 +18,9 @@ namespace ScrumPokerService.Hubs
             ICollection<User> participants = BusinessLogic.GetParticipants(id);
             Clients.Caller.getParticipants(participants);
 
-            String logText = "Get participants from room  " + id + " list asked by  " + BusinessLogic.GetUserNameByConnectionId(id, Context.ConnectionId) + "(" + Context.ConnectionId + ")";
+
+
+            String logText = "Get participants from room  " + id + " list asked by  " + BusinessLogic.GetUserNameByConnectionId(id, Context.ConnectionId) + "(" + Context.ConnectionId + "), response: " + String.Join("; ", participants);
             Trace.WriteLine(logText, "GetParticipants");
         }
 
@@ -58,6 +60,9 @@ namespace ScrumPokerService.Hubs
                 await Groups.Add(Context.ConnectionId, id.ToString());
                 ICollection<User> participants = BusinessLogic.GetParticipants(id);
                 Clients.Group(id.ToString()).getParticipants(participants);
+
+                String logText = "Room joined with id " + roomJoinedDTO.RoomId + " created by " + roomJoinedDTO.UserName + "(" + Context.ConnectionId + ")";
+                Trace.WriteLine(logText, "JoinRoom");
             }
         }
 
@@ -65,6 +70,9 @@ namespace ScrumPokerService.Hubs
         {
             ICollection<ProductBacklogItem> pbis = BusinessLogic.GetPBIs(id);
             Clients.Caller.getPBIs(pbis);
+
+            String logText = "PBI's from room " + id + " requested by  " + BusinessLogic.GetUserNameByConnectionId(id, Context.ConnectionId) + "(" + Context.ConnectionId + ")";
+            Trace.WriteLine(logText, "GetPBIs");
         }
 
         public void PushPBI(int id, string pbiName)
@@ -72,24 +80,36 @@ namespace ScrumPokerService.Hubs
             BusinessLogic.RemoveEstimates(id, pbiName);
             BusinessLogic.setRoomState(id, RoomState.Voting);
             Clients.Group(id.ToString()).pbiPushed(pbiName);
+
+            String logText = "PBI with name " + pbiName + " PUSHED in room " + id + " by " + BusinessLogic.GetUserNameByConnectionId(id, Context.ConnectionId) + "(" + Context.ConnectionId + ")";
+            Trace.WriteLine(logText, "PushPBI");
         }
 
         public void CreatePBI(int id, string title)
         {
             Boolean hasAdded = BusinessLogic.CreatePBI(id, title);
             Clients.Caller.createdPBI(hasAdded);
+
+            String logText = "PBI with name" + title + " CREATED in room " + id + " by " + BusinessLogic.GetUserNameByConnectionId(id, Context.ConnectionId) + "(" + Context.ConnectionId + ")";
+            Trace.WriteLine(logText, "CreatePBI");
         }
 
         public void UpdatePBI(int id, UpdatePBIDTO updateDTO)
         {
             Boolean isUpdated = BusinessLogic.UpdatePBI(id, updateDTO.OldTitle, updateDTO.NewTitle);
             Clients.Group(id.ToString()).updatedPBI(isUpdated);
+
+            String logText = "PBI changed name from " + updateDTO.OldTitle + " to " + updateDTO.NewTitle + " UPDATED in room " + id + " by " + BusinessLogic.GetUserNameByConnectionId(id, Context.ConnectionId) + "(" + Context.ConnectionId + ")";
+            Trace.WriteLine(logText, "UpdatePBI");
         }
 
         public void RemovePBI(int id, string title)
         {
             Boolean hasBeenRemoved = BusinessLogic.RemovePBI(id, title);
             Clients.Caller.removedPBI(hasBeenRemoved);
+
+            String logText = "PBI with name" + title + " REMOVED in room " + id + " by " + BusinessLogic.GetUserNameByConnectionId(id, Context.ConnectionId) + "(" + Context.ConnectionId + ")";
+            Trace.WriteLine(logText, "RemovePBI");
         }
 
         public void AddEstimation(int id, AddEstimateDTO addEstimateDTO)
@@ -103,17 +123,26 @@ namespace ScrumPokerService.Hubs
             }
 
             Clients.Group(id.ToString()).getUserEstimates(FindUserEstimates(id, addEstimateDTO.PBIName));
+
+            String logText = "Add estimate for " + addEstimateDTO.PBIName + " in room " + id + " by " + BusinessLogic.GetUserNameByConnectionId(id, Context.ConnectionId) + "(" + Context.ConnectionId + ")";
+            Trace.WriteLine(logText, "AddEstimation");
         }
 
         public void GetUserEstimates(int id, string title)
         {
             Clients.Caller.getUserEstimates(FindUserEstimates(id, title));
+
+            String logText = "Get user estimates for " + title +  " in room " + id +" by " + BusinessLogic.GetUserNameByConnectionId(id, Context.ConnectionId) + "(" + Context.ConnectionId + ")";
+            Trace.WriteLine(logText, "GetUserEstimates");
         }
 
         public void ShowEstimates(int id)
         {
             BusinessLogic.setRoomState(id, RoomState.Results);
             Clients.Group(id.ToString()).showEstimates();
+
+            String logText = "Show Estimates in room " + id + " by " + BusinessLogic.GetUserNameByConnectionId(id, Context.ConnectionId) + "(" + Context.ConnectionId + ")";
+            Trace.WriteLine(logText, "ShowEstimates");
         }
 
         public void SetFinalEstimate(int id, AddEstimateDTO finalEstimate)
@@ -123,6 +152,9 @@ namespace ScrumPokerService.Hubs
             {
                 BusinessLogic.setRoomState(id, RoomState.FinalEstimate);
                 Clients.Group(id.ToString()).finalEstimateSet(finalEstimate.Estimate);
+
+                String logText = "Final Estimate of " + finalEstimate.PBIName + " is SET in room " + id + " by " + BusinessLogic.GetUserNameByConnectionId(id, Context.ConnectionId) + "(" + Context.ConnectionId + ")";
+                Trace.WriteLine(logText, "SetFinalEstimate");
             }
             else
             {
@@ -134,8 +166,11 @@ namespace ScrumPokerService.Hubs
         {
             string finalEstimate = BusinessLogic.GetFinalEstimate(id, title);
             Clients.Caller.getFinalEstimate(finalEstimate);
-        }
 
+            String logText = "Get Final Estimate of " + title + " in room " + id + " by " + BusinessLogic.GetUserNameByConnectionId(id, Context.ConnectionId) + "(" + Context.ConnectionId + ")";
+            Trace.WriteLine(logText, "SetFinalEstimate");
+        }
+        //SAM
         public void AbortVoting(int id)
         {
             Clients.Group(id.ToString()).votingAborted();
